d00080beaba3a5f97cb237b551e446b3e0efe842
Louis Peter <louisclarencepeters@gmail.com>
2023-08-22 10:57:52 +0200

    added an if statement

--- server.js
+++ server.js
@@ -4,5 +4,12 @@ console.log("The app is runniong");
 
 dotenv.config();
 
-console.log("The app is runniong")
+console.log("The app is runniong");
 console.log(process.env);
+
+if (!process.env.HOST) {
+    console.log("Missing HOST env var");
+    process.exit();
+}
+
+console.log("The app is runniong"); 
