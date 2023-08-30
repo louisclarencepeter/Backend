2c07ee340b8a2ac51a441751258ca0b8ebd67f6d
Louis Peter <louisclarencepeters@gmail.com>
2023-08-30 11:18:56 +0200

    formatted the code

--- API-QUOTES/server.js
+++ API-QUOTES/server.js
@@ -1,20 +1,24 @@
-import express from 'express'; //importing the express library
+import express from "express"; //importing the express library
 
 const app = express(); // creating the an app which executing the express request
 
 //in-memory database
-
 const quotes = [
-    {quote:'It\'s not a bug; it\'s an undocumented feature.', author:'Anonynous'},
-    {quote:'Coding like poetry should be short and concise', author:'Santosh Kalwar'}
-]
+  {
+    quote: "It's not a bug; it's an undocumented feature.",
+    author: "Anonynous",
+  },
+  {
+    quote: "Coding like poetry should be short and concise",
+    author: "Santosh Kalwar",
+  },
+];
 
-app.get('/api/quotes', (req, res) => {
-    return res.send("welcome to the api");
+app.get("/api/quotes", (req, res) => {
+  return res.send(quotes);
 });
 
 //listen for requests pm port 3001
-app.listen(3001, ()=> {
-    console.log("The server is listening for requests...");
+app.listen(3001, () => {
+  console.log("The server is listening for requests...");
 });
-
