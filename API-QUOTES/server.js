66605ff3a780423f7ae283ca73d71376865d45a3
Louis Peter <louisclarencepeters@gmail.com>
2023-08-30 11:00:16 +0200

    installed the nodemon

--- API-QUOTES/server.js
+++ API-QUOTES/server.js
@@ -2,6 +2,7 @@ import express from 'express'; //importing the express library
 
 const app = express(); // creating the an app which executing the express request
 
+//listen for requests pm port 3001
 app.listen(3001, ()=> {
     console.log("The server is listening for requests...");
 });
