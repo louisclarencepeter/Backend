47e886a871e32a73be2f2dfe3b43ce1379f762e8
Louis Peter <louisclarencepeters@gmail.com>
2023-08-30 11:02:54 +0200

    added the an app.get

--- API-QUOTES/server.js
+++ API-QUOTES/server.js
@@ -2,6 +2,10 @@ import express from 'express'; //importing the express library
 
 const app = express(); // creating the an app which executing the express request
 
+app.get('/api/quotes', (req, res) => {
+    return res.send("welcome to the api");
+});
+
 //listen for requests pm port 3001
 app.listen(3001, ()=> {
     console.log("The server is listening for requests...");
