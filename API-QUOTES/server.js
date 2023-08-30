5f3db82e751c91e0b0e2b9473093fc11b5857526
Louis Peter <louisclarencepeters@gmail.com>
2023-08-30 11:15:08 +0200

    added an array of quotes

--- API-QUOTES/server.js
+++ API-QUOTES/server.js
@@ -2,6 +2,13 @@ import express from 'express'; //importing the express library
 
 const app = express(); // creating the an app which executing the express request
 
+//in-memory database
+
+const quotes = [
+    {quote:'It\'s not a bug; it\'s an undocumented feature.', author:'Anonynous'},
+    {quote:'Coding like poetry should be short and concise', author:'Santosh Kalwar'}
+]
+
 app.get('/api/quotes', (req, res) => {
     return res.send("welcome to the api");
 });
