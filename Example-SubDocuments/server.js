ab5e0e6fe8ce92b99feaad0dcdf14e185fe2a0e5
Louis Peter <louisclarencepeters@gmail.com>
2023-09-21 09:42:58 +0200

    added the .env file with details

--- Example-SubDocuments/server.js
+++ Example-SubDocuments/server.js
@@ -1,9 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
+
+app.use(express.json());
 
 app.listen(3001, (req, res) => {
   console.log("Server is running on port 3001");
 });
-
 
