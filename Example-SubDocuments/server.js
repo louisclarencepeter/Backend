3910c347b26222c4ef32192a8b33ea028f9a7c69
Louis Peter <louisclarencepeters@gmail.com>
2023-09-21 09:48:49 +0200

    added the connection to the database

--- Example-SubDocuments/server.js
+++ Example-SubDocuments/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -7,7 +8,18 @@ const app = express();
 
 app.use(express.json());
 
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}`
+  )
+  .then(() => {
+    console.log("Database connected! 😃");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    console.log("🤨");
+  });
+
 app.listen(3001, (req, res) => {
   console.log("Server is running on port 3001");
 });
-
